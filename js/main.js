f075dbfc081bf4e5cbfce7576e6c6018c22d3a84
Bogdan-jsx <sbogdan3755@gmail.com>
2022-12-28 19:21:35 +0200

    Animations to block about me

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 AOS.init()
 
-let navList = document.querySelector("nav ul");
+const navListElem = document.querySelector("nav ul");
+const technologiesListElem = document.querySelector(".technologies");
+const welcome = document.querySelector(".welcome");
+const navBar = document.querySelector("nav")
 
-console.log(navList)
+if (window.scrollY >= 970) {    // If when page loaded user don't see first block, it loads immediately without long animations
+    welcome.style.animationName = "none";
+    navListElem.style.animationName = "none";
+    navBar.style.animationName = "none";
 
-setTimeout(() => {
-    console.log(123)
-    navList.style.display = "flex";
-}, 6000)
+    welcome.style.paddingLeft = "15%";
+    welcome.style.width = "80%";
+
+    navBar.style.width = "22%";
+
+    navListElem.style.display = "flex";
+    navListElem.style.opacity = "1";
+} else {                        // Else it does an animation
+    setTimeout(() => {
+        navListElem.style.display = "flex";
+    }, 6000)
+}
+
+if (window.scrollY >= 350 && window.scrollY <= 1850) {     // If user looks at technologies list, it is visible, if he doesn't, it is not
+    technologiesListElem.style.display = "flex"
+} else {
+    technologiesListElem.style.display = "none"
+}
+
+window.onscroll = () => {
+    if (window.scrollY >= 350 && window.scrollY <= 1850) { // If user looks at technologies list, it is visible, if he doesn't, it is not (while scrolling)
+        technologiesListElem.style.display = "flex"
+    } else {
+        technologiesListElem.style.display = "none"
+    }
+}
